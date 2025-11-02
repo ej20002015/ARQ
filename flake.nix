1d70dd98c76af9d515eff5c1e20af5a8ca18237e
mulac <wallbridge.calum@gmail.com>
2025-11-02 16:59:28 +0000

    feat: nix build + docker

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "ARQ C++ project";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        shell  = import ./nix/shell.nix { inherit pkgs; };
+        arq = import ./nix/package.nix { inherit pkgs; };
+        docker = import ./nix/docker.nix { inherit pkgs arq; };
+      in {
+        devShells.default = shell;
+        packages = {
+          inherit arq docker;
+        };
+      }
+    );
+}
